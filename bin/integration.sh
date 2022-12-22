fe8c1025a1a3b3daa33ea5aa24c078efe83e2d77
Noah Callaway <ncallaway@gmail.com>
2022-12-22 00:22:38 -0800

    Add pre-push integration test hook

---
+++ bin/integration.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo -e "\nstarting integration tests\n"
+set -e
+
+docker-compose -f docker-compose.integration.yml up -d
+docker-compose -f docker-compose.integration.yml run --no-TTY local zsh /tests/runner.sh
+
+set +e
+docker-compose -f docker-compose.integration.yml down
